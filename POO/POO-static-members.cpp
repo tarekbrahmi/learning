dc7527f55aa37c288cb3ec2eebcf7480455ad61b
brahmitarek6@gmail.com <brahmitarek6@gmail.com>
2022-01-27 18:37:20 +0100

    static member POO in C++

--- POO/POO-static-members.cpp
+++ POO/POO-static-members.cpp
@@ -4,24 +4,37 @@ class A
 {
     // class for manupilate static membres(variable/functions)
     static int counter;
-    int myNumber;
+    int myNumber=0;
 
 public:
     void setMyNumber(void)
     {
-        myNumber += counter;
+        myNumber =++ counter;
     }
     void displayMynumber(void)
     {
         cout << "My number value :" << myNumber << endl;
     }
-    void displayMynumber(void)
+    static void displayCounter(void)
     {
         cout << "Counter value :" << counter << endl;
     }
 };
+int A::counter;
 int main()
 {
+    A a,aa;
+    a.setMyNumber();
+    aa.setMyNumber();
+
+    a.displayMynumber();
+    aa.displayMynumber();
+    
+    A::displayCounter();
+    A aaa;
+    aaa.setMyNumber();
+    aaa.displayMynumber();
+    A::displayCounter();
 
     return 0;
 }
