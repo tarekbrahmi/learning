7ab91e37c1e9098ac3afb1fb2a168b588d12fac5
brahmitarek6@gmail.com <brahmitarek6@gmail.com>
2022-01-27 17:05:03 +0100

    POO in c++

--- POO/POO-first.cpp
+++ POO/POO-first.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 class PERSON
 {
     int age;
-    char name[33];
+    char name[2];
+
+private:
+    char summray[];
 
 public:
     void setData(void);
@@ -20,11 +24,26 @@ void PERSON::setData(void)
     cin >> name;
     cout << "Enter age" << endl;
     cin >> age;
+    sprintf(summray,"Name %s age %d", name, age);
+    cout << summray << endl;
 }
 int main()
 {
-    PERSON p;
-    p.setData();
-    p.displayData();
+    PERSON persons[1];
+    int i = 0;
+    while (i < 1)
+    {
+        cout << "Enter data for person " << i + 1 << endl;
+        persons[i].setData();
+
+        i++;
+    }
+    int j = 0;
+    while (j < 1)
+    {
+        cout << "data for person " << j + 1 << endl;
+        persons[j].displayData();
+        j++;
+    }
     return 0;
 }
