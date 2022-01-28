a5dbb810058f190268845dc65a1f95165a4260f8
brahmitarek6@gmail.com <brahmitarek6@gmail.com>
2022-01-28 16:31:47 +0100

    destructor POO in C++

---
+++ POO/POO-operator-overloading.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class MyClass
+{
+    // this class is used to manipulate operators
+public:
+    MyClass();
+};
+
+int main()
+{
+    MyClass my;
+    return 0;
+}
