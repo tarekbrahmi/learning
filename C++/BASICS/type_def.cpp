90696666d682d3732d5795ecbe487837ea35dac9
tarekbrahmi <brahmitarek6@gmail.com>
2022-11-24 16:10:59 +0100

    function pointer with typedef

--- C++/BASICS/type_def.cpp
+++ C++/BASICS/type_def.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+// Normal pointer to a function
+int (*func_ptr1)(int, int);
+  
+// Using typedef with pointer to a function
+typedef int (*func_ptr2)(int, int);
+  
+// Function to multiply two numbers
+int product(int u, int v) { return u * v; }
+
 int main()
 {
     // MyVector used to define new vector of integers
@@ -30,6 +40,20 @@ int main()
     printf("data of integer pointer %d\n", *iptr);
 
     // typedef with function pointer
+    func_ptr1 = &product;
+  
+    // Using typedefed function pointer for creating new
+    // function pointer "new_func"
+    func_ptr2 new_func_ptr = &product;
+  
+    // Using normal pointer to a function
+    int x2 = (*func_ptr1)(3, 2);
+  
+    // Using the new function pointer
+    int x1 = (*new_func_ptr)(2, 4);
+  
+    std::cout << x1 << std::endl;
+    std::cout << x2 << std::endl;
     
     return 0;
 }
