52e6934ccaaf7e8c63ffda53bbe8ae557a329e11
tarekbrahmi <brahmitarek6@gmail.com>
2022-11-25 22:36:23 +0100

    build library

--- C++/CMAKE_TUTO/example1/main.cpp
+++ C++/CMAKE_TUTO/example1/main.cpp
@@ -2,14 +2,12 @@
 #ifndef SYSTEM
 #define SYSTEM "NOT_DEFINED"
 #endif
-auto sum(int a, int b)
-{
-    return a + b;
-}
+#include "lib/MyLib/stuff.hpp"
 int main()
 {
+    stuff::operations op;
     std::cout << "Hello World!" << std::endl;
     std::cout << "Im using : " << SYSTEM << std::endl;
-    std::cout << "Sum of 3 + 4 : " << sum(3, 4) << std::endl;
+    std::cout << "Sum of 3 + 4 : " << op.sum(3, 4) << std::endl;
     return 0;
 }
