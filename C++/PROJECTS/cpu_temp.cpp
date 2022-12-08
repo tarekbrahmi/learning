6a18da82ee63f99f18b416b9290fe93eb73e9f83
tarekbrahmi <brahmitarek6@gmail.com>
2022-12-08 17:17:45 +0100

    yes cpu usage works

--- C++/PROJECTS/cpu_temp.cpp
+++ C++/PROJECTS/cpu_temp.cpp
@@ -50,9 +50,57 @@ public:
 };
 class CPU_USAGE : public CMD
 {
-
+    // [pc@fedora PROJECTS]$ sh ./cpu_usage.sh
+    // --- First callc 672060 3039 212188 4115342 251246 57066 63903 0 0 0
+    // --- Make summ cpu_sum=5374844
+    // --- Make cpu_delta 5374844
+    // --- Make cpu_idle cpu , ,4115342
+    // --- Make cpu_used 5374844 4115342
+    // --- Calc usage 100 * cpu_used / cpu_delta)
+    // --- Keeeep values cpu_last_sum=5374844 cpu_last=(cpu 672060 3039 212188 4115342 251246 57066 63903 0 0 0)
+    // CPU usage at 23%
+    // --- First callc 672102 3040 212208 4115673 251246 57070 63904 0 0 0
+    // --- Make summ cpu_sum=5375243
+    // --- Make cpu_delta 5375243 5374844
+    // --- Make cpu_idle cpu , cpu ,331
+    // --- Make cpu_used 399 331
+    // --- Calc usage 100 * cpu_used / cpu_delta)
+    // --- Keeeep values cpu_last_sum=5375243 cpu_last=(cpu 672102 3040 212208 4115673 251246 57070 63904 0 0 0)
 public:
     CPU_USAGE(){};
+    double CPU_usage_V2(unsigned long long &cpu_sum, unsigned long long &cpu_idle)
+    {
+        double percent = 0;
+        unsigned long long user, nice, system, idle, iowait, irq, softirq;
+        unsigned long long _cpu_sum = 0;
+        unsigned long long _cpu_idle = 0;
+        unsigned long long cpu_delta = 0;
+        unsigned long long cpu_used = 0;
+
+        FILE *file = fopen(PATH_CPU_STATE, "r");
+        fscanf(file, "cpu %llu %llu %llu %llu %llu %llu %llu", &user, &nice,
+               &system, &idle, &iowait, &irq, &softirq);
+        fclose(file);
+        // callc the summ
+        _cpu_sum = user + nice + system + idle + iowait + irq + softirq;
+        _cpu_idle = idle - cpu_idle;
+
+        // calc cpu_delta
+        cpu_delta = _cpu_sum - cpu_sum;
+
+        // calc cpu_used
+        cpu_used = cpu_delta - _cpu_idle;
+
+        percent = cpu_used;
+        percent /= cpu_delta;
+        percent *= 100;
+        cout << "usage percent " << percent << endl;
+        // save the value
+        cpu_sum = _cpu_sum;
+        cpu_idle = idle;
+
+        return percent;
+    }
     double Cpu_Usage()
     {
         double percent;
@@ -68,6 +116,7 @@ public:
         return percent;
     }
 };
+
 int main()
 {
     char cmd_tump[255];
@@ -77,11 +126,15 @@ int main()
     CMD CPU_CMD = CMD();
     CPU_USAGE _CPU_USAGE = CPU_USAGE();
     UTIL util = UTIL();
+    unsigned long long cpu_sum = 0;
+    unsigned long long cpu_idle = 0;
+
     while (true)
     {
 
+        _CPU_USAGE.CPU_usage_V2(cpu_sum, cpu_idle);
         CPU_CMD.exec(cmd_sleep);
-        cout << _CPU_USAGE.Cpu_Usage() << endl;
     }
+
     return 0;
 }
